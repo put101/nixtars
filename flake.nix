fd285b15a0771b645a85999f9e4b645db1428e82
Tobias NixTARS <tobiaspucher@gmail.com>
2025-09-19 03:09:25 +0200

    fixed flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,24 @@
 {
-  description = "A very basic flake";
+  description = "Nixos config flake";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs }: {
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    # use "nixos", or your hostname as the name of the configuration
+    # it's a better practice than "default" shown in the video
     nixosConfigurations.nixtars = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/default/configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
     };
   };
 }
