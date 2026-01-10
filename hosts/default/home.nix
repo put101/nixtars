466a4f7661a631fb6c4bee9088786b34ca98505a
Tobias NixOS Lenovo <tobiaspucher@gmail.com>
2026-01-10 13:18:55 +0100

    grim, flameshot

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -66,43 +66,7 @@
   #
   # or
   #  programs.fish.enable = true;
-  #  /etc/profiles/per-user/tobi/etc/profile.d/services.flameshot = {
-  enable = true;
-  settings = {
-    General = {
-
-      # More settings may be found on the Flameshot Github
-
-      # Save Path
-      savePath = "/home/user/Screenshots";
-      # Tray
-      disabledTrayIcon = true;
-      # Greeting message
-      showStartupLaunchMessage = false;
-      # Default file extension for screenshots (.png by default)
-      saveAsFileExtension = ".png";
-      # Desktop notifications
-      showDesktopNotification = true;
-      # Notification for cancelled screenshot
-      showAbortNotification = false;
-      # Whether to show the info panel in the center in GUI mode
-      showHelp = true;
-      # Whether to show the left side button in GUI mode
-      showSidePanelButton = true;
-
-
-      # Color Customization
-      uiColor = "#740096";
-      contrastUiColor = "#270032";
-      drawColor = "#ff0000";
-
-      # For Wayland (Install Grim seperately)
-      useGrimAdapter = true;
-      # Stops warnings for using Grim
-      disabledGrimWarning = true;
-    };
-  };
-};hm-session-vars.sh
+  #  /etc/profiles/per-user/tobi/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
     EDITOR = "nvim";
@@ -140,7 +104,6 @@
    direnv
    nix-direnv
   ];
-
 
   services.flameshot = {
     enable = true;
@@ -183,5 +146,4 @@
       };
     };
   };
-
 }
