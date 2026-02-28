c999545673dcaf818405684255a0668d5387264b
Tobias <tobiaspucher@gmail.com>
2026-02-28 13:57:23 +0100

    fix(nvim): remove broken spellfile auto-download to stop startup message

--- hosts/default/nvim-core/after/plugin/spellfix.lua
+++ hosts/default/nvim-core/after/plugin/spellfix.lua
@@ -1,19 +1,11 @@
 local spell_dir = vim.fn.stdpath("data") .. "/spell"
 vim.fn.mkdir(spell_dir, "p")
+
+-- Only set spellfile and spelllang if the files exist.
+-- To get the 'programming' dictionary, add 'psliwka/vim-dirtytalk' to your plugins.
 vim.opt.spellfile = spell_dir .. "/en.utf-8.add"
-if vim.fn.executable("curl") == 1 then
-  local programming_spell = spell_dir .. "/programming.utf-8.spl"
-  if vim.fn.filereadable(programming_spell) == 0 then
-    vim.notify("Downloading programming spell file...", vim.log.levels.INFO)
-    vim.fn.system({
-      "curl", "-fsSL",
-      "-o", programming_spell,
-      "https://raw.githubusercontent.com/psliwka/vim-dirtytalk/master/spell/programming.utf-8.spl"
-    })
-    if vim.v.shell_error == 0 then
-      vim.opt.spelllang:append("programming")
-    end
-  else
-    vim.opt.spelllang:append("programming")
-  end
+
+local programming_spell = spell_dir .. "/programming.utf-8.spl"
+if vim.fn.filereadable(programming_spell) == 1 then
+  vim.opt.spelllang:append("programming")
 end
