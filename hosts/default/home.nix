fbd863ef4e4999953c144cde919e8eecb7178622
Tobias <tobiaspucher@gmail.com>
2026-02-28 13:49:45 +0100

    feat(nvim): link nvim-kickstart as default nvim config for nightly

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -183,6 +183,7 @@ in {
   # Note: this file is deployed via Home Manager to `~/.config/niri/config.kdl`.
   # Changes in `./config.kdl` only take effect after `nixos-rebuild switch` (or `home-manager switch`).
   xdg.configFile."niri/config.kdl".source = lib.mkForce ./config.kdl;
+  xdg.configFile."nvim".source = ./nvim-kickstart;
   # xdg.configFile."nvim-kickstart".source = ./nvim-kickstart; (soft disabled for now)
   # xdg.configFile."nvim-mine".source = ./nvim-mine; (soft disabled for now)
 
@@ -219,30 +220,6 @@ in {
     ./pia.nix
     # ./custom-neovim.nix (soft disabled for now)
   ];
-
-  xdg.configFile."nvim/after/plugin/spellfix.lua" = {
-    text = ''
-      local spell_dir = vim.fn.stdpath("data") .. "/spell"
-      vim.fn.mkdir(spell_dir, "p")
-      vim.opt.spellfile = spell_dir .. "/en.utf-8.add"
-      if vim.fn.executable("curl") == 1 then
-        local programming_spell = spell_dir .. "/programming.utf-8.spl"
-        if vim.fn.filereadable(programming_spell) == 0 then
-          vim.notify("Downloading programming spell file...", vim.log.levels.INFO)
-          vim.fn.system({
-            "curl", "-fsSL",
-            "-o", programming_spell,
-            "https://raw.githubusercontent.com/psliwka/vim-dirtytalk/master/spell/programming.utf-8.spl"
-          })
-          if vim.v.shell_error == 0 then
-            vim.opt.spelllang:append("programming")
-          end
-        else
-          vim.opt.spelllang:append("programming")
-        end
-      end
-    '';
-  };
 
   # programs.nvf moved to ./nvf.nix
 
